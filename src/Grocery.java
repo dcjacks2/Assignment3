5cbf5b5c708f0a27dce558047d9f87ed76ba689a
dcjacks2 <bockielou@hotmail.com>
2019-11-07 20:56:37 -0600

    adding private variables and constructor

--- src/Grocery.java
+++ src/Grocery.java
@@ -1,2 +1,13 @@
 public class Grocery {
+    private double price;
+    private double units;
+    private double discount;
+
+    public Grocery(double p, double u, double d){
+        price = p;
+        units = u;
+        discount = d;
+    }
+
+
 }
